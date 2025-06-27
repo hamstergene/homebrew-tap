2f95fd0f90a0414f1dd03f59500f24b3278aa22d
Eugene Homyakov <ehomyakov@gmail.com>
2025-06-26 23:07:43 -0700

    update Python formulas to match 3.12+ venv requirements

--- findold.rb
+++ findold.rb
@@ -1,16 +1,20 @@
 require "formula"
 
 class Findold < Formula
+    include Language::Python::Virtualenv
+
     desc "Find files older than given number of days, optionally moving or deleting them."
     url "https://github.com/hamstergene/findold", :using => :git, :tag => "v1.0"
-    version "1.0"
+    version "1.1"
     sha256 'b076626895291679627c78163ad8b523831b71b5a38f3239065555a987b97e48'
     homepage 'https://github.com/hamstergene/findold'
 
-    depends_on "python3"
+    depends_on "python@3"
 
     def install
-        bin.install "findold"
+        virtualenv_create(libexec, "python3")
+        libexec.install "findold"
+        bin.install_symlink libexec/"findold"
     end
 end
 
--- p5.rb
+++ p5.rb
@@ -1,15 +1,19 @@
 require "formula"
 
 class P5 < Formula
-    url 'https://github.com/hamstergene/p5.git', :tag => 'v0.8'
+  include Language::Python::Virtualenv
+
+  url 'https://github.com/hamstergene/p5.git', :tag => 'v0.8'
   sha256 'f3c87969b65c4689f22b47f0a1273120f511ccd8fba64442734ff11340a42c12'
-  version '0.8'
+  version '0.9'
   homepage 'https://github.com/hamstergene/p5'
 
-  depends_on "python3"
+  depends_on "python@3"
 
   def install
-    bin.install "p5"
+    virtualenv_create(libexec, "python3")
+    libexec.install "p5"
+    bin.install_symlink libexec/"p5"
   end
 end
 
